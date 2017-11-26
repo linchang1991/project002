39e2b62fc9f86519308b73b83094079a08295011
linchang <479780364@qq.com>
2017-11-26 18:04:00 +0800

    -ALLOWED_HOSTS = ['120.79.38.21'] +ALLOWED_HOSTS = ['120.79.38.21','127.0.0.1']

--- project002/settings.py
+++ project002/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'a+_1t!3u)w(-3qpkqxcp%ge-9px=ku+8izuvfyo0@)83p8pym7'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
-ALLOWED_HOSTS = ['120.79.38.21']
+ALLOWED_HOSTS = ['120.79.38.21','127.0.0.1']
 
 #DEBUG = False
 #ALLOWED_HOSTS = ['*']
